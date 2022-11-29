af78a7e4d3630c2c098feffa7f3e11f4cc85cbaf
Alexandru Dumitru <alexandru666@outlook.es>
2022-11-29 01:08:53 +0100

    Final movement script

--- 3D dot Game Hero/Assets/Scripts/Input/PlayerInput.cs
+++ 3D dot Game Hero/Assets/Scripts/Input/PlayerInput.cs
@@ -55,46 +55,24 @@ public class PlayerInput : MonoBehaviour {
     }
 
     private void movementRoutine() {
-        transform.Translate(new Vector3(moveDirection.y * moveSpeed.x, 0.0f, -1.0f * moveDirection.x * moveSpeed.y) * Time.deltaTime, Space.World);
-        setRotation();
+        transform.Translate(new Vector3(    ((moveDirection.y != 0.0f) ? Mathf.Sign(moveDirection.y) : 0.0f) * moveSpeed.x, 
+                                            0.0f, 
+                                            -1.0f * ((moveDirection.x != 0.0f) ? Mathf.Sign(moveDirection.x) : 0.0f) * moveSpeed.y) 
+                            * Time.deltaTime, Space.World);
 
+        setRotation();
         playerAnimator.SetBool("Running", moveDirection != Vector2.zero);
     }
 
-    /**
-     * Both angle1 and angle2 should go with a range of [-180,180)
-     * @params angle1 
-     **/
-    /// <summary> Return the direction to the shortest rotation into destination </summary>
-    /// <param name="origin"> Origin of the rotation</param>
-    /// <param name="destination"> Destination of the rotation</param>
-    /// <remarks> Both angle should have a range of [-180,180)</remarks>
-    private float getShortestSign(float origin, float destination) {
-
-        return 0.0f;
-    }
-
     private void rotationRoutine() {
-        float angle = -180.0f + ((int)currentRotation * 45.0f);
-        getShortestSign(angle, angle);
-        transform.rotation = Quaternion.Euler(new Vector3(0.0f,angle,0.0f));
-        /*if (currentRotation == 0) angle = (transform.rotation.y > 180.0f ? 360.0f : 0.0f);
-
-        float _partialRotation = angle - transform.rotation.y;
-        float sign = _partialRotation >= 0.0f ? 1.0f : -1.0f;
-
-        Vector3 _rotation = new Vector3(0.0f,Mathf.Abs(_partialRotation) > rotationSpeed ? sign * rotationSpeed * Time.deltaTime : _partialRotation * Time.deltaTime,0.0f);
-        
-        transform.Rotate(_rotation, Space.World);*/
+        float angleDest = -180.0f + ((int)currentRotation * 45.0f);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, angleDest, 0.0f), rotationSpeed);
     }
 
-    void Start()
-    {
-        
+    void Start(){
     }
 
     void Update() {
-
         moveDirection = move.ReadValue<Vector2>();
         fireDirection = fire.ReadValue<Vector2>();
 
