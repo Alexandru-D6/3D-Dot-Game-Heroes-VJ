5ab21bb8bc0633b607212019b1182f3f4c81ee48
Alexandru Dumitru <alexandru666@outlook.es>
2022-12-08 16:22:07 +0100

    last changes

--- 3D dot Game Hero/Assets/Scripts/Debug/DebugControls.cs
+++ 3D dot Game Hero/Assets/Scripts/Debug/DebugControls.cs
@@ -10,8 +10,6 @@ public class DebugControls : MonoBehaviour {
     public WeaponManager weaponManager;
     public WeaponScript weaponScript;
 
-    [SerializeField] private string weaponToSpawn;
-
     [SerializeField] private bool buttonAvailability;
     [SerializeField] private float buttonDelay;
 
@@ -28,25 +26,21 @@ public class DebugControls : MonoBehaviour {
     public void Key_1_Pressed(InputAction.CallbackContext context) {
         if (buttonAvailability) {
             StartCoroutine(delayedButon(buttonDelay));
-            weaponManager.DB_selectWeapon(weaponToSpawn);
         }
     }
     public void Key_2_Pressed(InputAction.CallbackContext context) {
         if (buttonAvailability) {
             StartCoroutine(delayedButon(buttonDelay));
-            weaponManager.DB_unselectCurrentWeapon();
         }
     }
     public void Key_3_Pressed(InputAction.CallbackContext context) {
         if (buttonAvailability) {
             StartCoroutine(delayedButon(buttonDelay));
-            weaponManager.DB_deleteWeapon(weaponToSpawn);
         }
     }
     public void Key_4_Pressed(InputAction.CallbackContext context) {
         if (buttonAvailability) {
             StartCoroutine(delayedButon(buttonDelay));
-            weaponManager.DB_createWeapon(weaponToSpawn, int.MaxValue);
         }
     }
     public void Key_5_Pressed(InputAction.CallbackContext context) {
@@ -62,7 +56,7 @@ public class DebugControls : MonoBehaviour {
     public void Key_7_Pressed(InputAction.CallbackContext context) {
         if (buttonAvailability) {
             StartCoroutine(delayedButon(buttonDelay));
-            weaponScript = GameObject.FindGameObjectWithTag(weaponToSpawn).GetComponent<WeaponScript>();
+            weaponScript = GameObject.FindGameObjectWithTag("Sword").GetComponent<WeaponScript>();
         }
     }
     public void Key_8_Pressed(InputAction.CallbackContext context) {
@@ -74,7 +68,6 @@ public class DebugControls : MonoBehaviour {
     public void Key_9_Pressed(InputAction.CallbackContext context) {
         if (buttonAvailability) {
             StartCoroutine(delayedButon(buttonDelay));
-            Debug.Log(weaponScript.GetLevelOfPower() + 1);
             weaponScript.SetLevelOfPower(weaponScript.GetLevelOfPower() + 1);
         }
     }
