641247e94730f52f6f2c7c70db5ca8aca72eda2d
Alexandru Dumitru <alexandru666@outlook.es>
2022-12-08 16:50:47 +0100

    creating chest prefab

---
+++ 3D dot Game Hero/Assets/Scripts/Objects/Chest/ChestScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestScript : MonoBehaviour {
+
+#region Parameters
+
+
+
+#endregion
+
+#region MonoBehaviour Methods
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+#endregion
+
+}
