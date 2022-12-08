aeb292ea91b33d5709afe305e257bc58b134c42f
Alexandru Dumitru <alexandru666@outlook.es>
2022-12-08 19:42:22 +0100

    Chest implementation (Part #1)

--- 3D dot Game Hero/Assets/Scripts/Objects/Chest/ChestScript.cs
+++ 3D dot Game Hero/Assets/Scripts/Objects/Chest/ChestScript.cs
@@ -1,19 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ChestScript : MonoBehaviour {
 
 #region Parameters
 
+    [Header("Managers")]
+    [SerializeField] private ChestAnimations chestAnimations;
 
+    [SerializeField] private List<GameObject> prefabs;
+    [SerializeField] private bool isPlayerNear;
+    private bool canOpen;
+    [SerializeField] private float openDelay;
+
+#endregion
+
+#region IEnumerators
+
+    IEnumerator delayedOpen(float time) {
+        yield return new WaitForSeconds(time);
+        canOpen = true;
+    }
+
+#endregion
+
+#region Public Methods
+
+    public void playerEntered(bool value) {
+        isPlayerNear = value;
+    }
 
 #endregion
 
 #region MonoBehaviour Methods
 
     void Start() {
-        
+        canOpen = true;
     }
 
     void Update() {
@@ -21,5 +45,17 @@ public class ChestScript : MonoBehaviour {
     }
 
 #endregion
+
+    #region Callbacks Functions
+
+    public void OpenChest(InputAction.CallbackContext context) {
+        if (canOpen) {
+            canOpen = false;
+            Debug.Log("Chest Opened");
+            StartCoroutine(delayedOpen(openDelay));
+        }
+    }
+
+    #endregion
 
 }
