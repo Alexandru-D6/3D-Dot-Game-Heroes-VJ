ae73e84619112e05aacf086f700166d386699640
arubio <alvaro.alumne.tcn@gmail.com>
2022-12-13 02:13:34 +0100

    raycast layer enemies

---
+++ 3D dot Game Hero/Assets/Scripts/Player/PlayerRayCast.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRayCast : MonoBehaviour
+{
+    [SerializeField] LayerMask layerMask;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hitInfo, 20f, layerMask)) 
+        {
+            //hitInfo.transform;
+        }
+    }
+}
