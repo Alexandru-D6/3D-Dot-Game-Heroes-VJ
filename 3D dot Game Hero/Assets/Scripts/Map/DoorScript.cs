b09cb651a76bcfbb0c186ff2a57dc09ecbc3f159
Alexandru Dumitru <alexandru666@outlook.es>
2022-12-28 17:35:07 +0100

    Door implementation I

--- 3D dot Game Hero/Assets/Scripts/Map/DoorScript.cs
+++ 3D dot Game Hero/Assets/Scripts/Map/DoorScript.cs
@@ -4,11 +4,56 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour {
 
-    [SerializeField] private bool doorOpened = false;
+#region Parameters
+
+    [Header("References")]
     [SerializeField] private Animator animator;
+
+    [Header("Objects")]
+    [SerializeField] private GameObject padlock;
+
+    [Header("Configuration")]
+    // Maybe remove it?
+    [SerializeField] private Vector3 padlockPosition;
+    [SerializeField] private Vector3 padlockRotation;
+
+    [Header("States")]
+    [SerializeField] private bool doorOpened = false;
+    [SerializeField] private bool playerNearby = false;
+
+#endregion Parameters
+
+#region Collisions Methods
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == Tags.Player.ToString()) playerNearby = true;
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == Tags.Player.ToString()) playerNearby = false;
+    }
+
+#endregion
+
+#region Private Methods
+
+
+
+#endregion
+
+#region Private Methods
+
+
+
+#endregion
+
+#region MonoBehaviour Methods
 
     private void Update() {
         animator.SetBool("Open", doorOpened);
+        padlock.SetActive(!doorOpened);
     }
+
+#endregion MonoBehaviour Methods
 
 }
