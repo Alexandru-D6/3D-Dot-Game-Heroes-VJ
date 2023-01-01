f9863fc085ca7d5ead4947cf26d4d1634b728c23
Alvarorodri <66634603+Alvarorodri@users.noreply.github.com>
2023-01-01 14:01:07 +0100

    [Feature] add particles padlock and movable column (#49)
    * added particles
    
    * particles
    
    * new prefab addded
    
    * column movable
    
    * a

--- 3D dot Game Hero/Assets/Scripts/Utilities/Tags.cs
+++ 3D dot Game Hero/Assets/Scripts/Utilities/Tags.cs
@@ -33,6 +33,7 @@ public enum Tags {
     SpawnParticles,
     EndermanArm,
     Debug,
+    Obstacle,
     ExplotionBomb,
     Uknown,
 }
@@ -73,6 +74,17 @@ public enum Layers{
     Weapon
 }
 
+[System.Serializable] 
+public enum ObstacleParts{ 
+    Front,
+    Back,
+    Right,
+    Left,
+    Up,
+    Down
+}
+// Start is called before the first frame update
+
 public class TagsUtils {
     public static Tags GetTag(string name) {
         return (Tags)System.Enum.Parse( typeof(Tags), name );
@@ -81,4 +93,10 @@ public class TagsUtils {
     public static Layers GetLayers(string name) {
         return (Layers)System.Enum.Parse( typeof(Layers), name );
     }
+
+    public static ObstacleParts GetObstaclePart(string name)
+    {
+        return (ObstacleParts)System.Enum.Parse(typeof(ObstacleParts), name);
+    }
+
 }
