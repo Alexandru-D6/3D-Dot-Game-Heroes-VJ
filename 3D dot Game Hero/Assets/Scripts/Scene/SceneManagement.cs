b8ac3a6e94f1d978cd3a74934a952fb9bc4cc339
arubio <alvaro.alumne.tcn@gmail.com>
2023-01-02 02:06:28 +0100

    added credits and menu

--- 3D dot Game Hero/Assets/Scripts/Scene/SceneManagement.cs
+++ 3D dot Game Hero/Assets/Scripts/Scene/SceneManagement.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneManager : MonoBehaviour {
+public class SceneManagement : MonoBehaviour {
 
     #region Singleton
 
-    public static SceneManager Instance { get; private set; }
+    public static SceneManagement Instance { get; private set; }
 
     private void Awake() {
         if (Instance != null && Instance != this) Destroy(this);
