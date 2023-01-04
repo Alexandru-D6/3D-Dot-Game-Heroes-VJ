81679d9472b3372154084f1df853868fb6d19e20
Alexandru Dumitru <alexandru666@outlook.es>
2023-01-04 16:26:09 +0100

    Room finished and camera movement

---
+++ 3D dot Game Hero/Assets/Scripts/Map/BossRoom Script/BossRoomEntry.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossRoomEntry : MonoBehaviour {
+
+    [SerializeField] Camera playerCamera;
+    [SerializeField] Vector3 CameraPosition;
+
+    [SerializeField] List<GridScript> gridsInsideTheRoom;
+
+    IEnumerator delayedGridDoorsRoutine(float time, bool state) {
+        yield return new WaitForSeconds(time);
+
+        if (state) UnlockGrids();
+        else LockGrids();
+    }
+
+    private void UnlockGrids() {
+        foreach(var grid in gridsInsideTheRoom) {
+            if (grid != null && grid.gameObject.activeInHierarchy) grid.OpenGrid();
+        }
+    }
+
+    private void LockGrids() {
+        foreach(var grid in gridsInsideTheRoom) {
+            if (grid != null && grid.gameObject.activeInHierarchy) grid.CloseGrid();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        playerCamera.GetComponent<CameraSmoothMovement>().MoveTo(CameraPosition);
+        StartCoroutine(delayedGridDoorsRoutine(1.0f, false));
+    }
+
+    private void Start() {
+        playerCamera = Camera.main;
+        UnlockGrids();
+    }
+
+}
